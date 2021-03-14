7e3adb4d6224a8cb27b94e2887bcf7a95cb15951
ywwsxya <ywwsxya@gmail.com>
2021-03-14 17:20:12 +0800

    Update nav.config.ts
    copy main nav.config

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/ywwsxya/nav',
 
   // 部署分支
   branch: 'own',
@@ -14,24 +14,24 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://19970310.xyz',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: 'upup导航',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: 'upup自用导航',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '导航,HACK,CTF,WEB,实用工具,PYTHON,爬虫,网络安全,面试',
 
   // 默认主题: Light | Sim | Side | App
-  theme: 'Side',
+  theme: 'Light',
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2018-2021 19970310.xyz, All Rights Reserved</div>
   `,
 
   // 百度统计地址
@@ -43,6 +43,50 @@ const c: IConfig = {
   cnzzStatisticsUrl: '',
 
   // Light 主题配置
+  lightThemeConfig: {
+    // https://www.19970310.xyz/#/light?q=grabient
+    backgroundLinear: [
+      'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)',
+      'linear-gradient(90deg, #FEE140 0%, #FA709A 100%)',
+      'linear-gradient(0deg, #08AEEA 0%, #2AF598 100%)',
+      'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)',
+      'linear-gradient(19deg, #FAACA8 0%, #DDD6F3 100%)',
+      'linear-gradient(147deg, #FFE53B 0%, #FF2525 74%)',
+      'linear-gradient(180deg, #52ACFF 25%, #FFE32C 100%)',
+      'linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)',
+      'linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%)',
+      'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%)',
+      'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)',
+      'linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%)',
+      'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+      'linear-gradient(90deg, #FAD961 0%, #F76B1C 100%)',
+      'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+      'linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%)',
+      'linear-gradient(90deg, #FF9A8B 0%, #FF6A88 55%, #FF99AC 100%)',
+      'linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)',
+      'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
+      'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
+      'linear-gradient(180deg, #A9C9FF 0%, #FFBBEC 100%)',
+      'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+      'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)',
+      'linear-gradient(132deg, #F4D03F 0%, #16A085 100%)',
+      'linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)',
+      'linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)'
+    ]
+  },
+
+  // Sim 主题配置
+  simThemeConfig: {
+    // 海报图
+    posterImageUrls: [
+      'https://raw.sevencdn.com/ywwsxya/nav/image/sim-wallpaper.jpg'
+    ],
+    description: 'my hack 工具站，收录<b>${total}</b> 个优质网站'
+  }
+}
+
+export default c
+
   lightThemeConfig: {
     // https://www.nav3.cn/#/light?q=grabient
     backgroundLinear: [
