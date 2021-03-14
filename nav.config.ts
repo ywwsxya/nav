cd0adef176ce5fc1e478c008d4b521a24e9b120a
ywwsxya <ywwsxya@gmail.com>
2021-03-14 16:10:10 +0800

    Update nav.config.ts
    小更改

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/ywwsxya/nav',
 
   // 部署分支
   branch: 'main',
@@ -14,16 +14,16 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://19970310.xyz',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: 'upup导航',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: 'upup自用导航',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '导航,HACK,CTF,WEB,实用工具,PYTHON,爬虫,网络安全,面试',
 
   // 默认主题: Light | Sim | Side | App
   theme: 'Light',
@@ -31,7 +31,7 @@ const c: IConfig = {
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2018-2021 19970310.xyz, All Rights Reserved</div>
   `,
 
   // 百度统计地址
@@ -44,7 +44,7 @@ const c: IConfig = {
 
   // Light 主题配置
   lightThemeConfig: {
-    // https://www.nav3.cn/#/light?q=grabient
+    // https://www.19970310.xyz/#/light?q=grabient
     backgroundLinear: [
       'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)',
       'linear-gradient(90deg, #FEE140 0%, #FA709A 100%)',
@@ -79,9 +79,9 @@ const c: IConfig = {
   simThemeConfig: {
     // 海报图
     posterImageUrls: [
-      'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
+      'https://raw.sevencdn.com/ywwsxya/nav/image/sim-wallpaper.jpg'
     ],
-    description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
+    description: 'my hack 工具站，收录<b>${total}</b> 个优质网站'
   }
 }
 
